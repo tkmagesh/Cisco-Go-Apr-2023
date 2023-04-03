25830837ace800703e57bc0e9f96b5cea941c753
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-03 12:29:48 +0530

    variable declaration choices

--- 01-intro/02-variables.go
+++ 01-intro/02-variables.go
@@ -24,5 +24,86 @@ func main() {
 
 	name := "Magesh"
 	fmt.Printf("Hi %s!\n", name)
+	fmt.Println(len(name))
+
+	// Using multiple variables
+	/*
+		var x int
+		var y int
+		var result int
+		var str string
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var x, y, result int
+		var str string
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y, result int
+			str          string
+		)
+		x = 100
+		y = 200
+		result = x + y
+		str = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var x int = 100
+		var y int = 200
+		var result int = x + y
+		var str string = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var x, y int = 100, 200
+		var result int = x + y
+		var str string = "Sum of %d and %d is %d\n"
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y   int    = 100, 200
+			result int    = x + y
+			str    string = "Sum of %d and %d is %d\n"
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y   = 100, 200
+			result = x + y
+			str    = "Sum of %d and %d is %d\n"
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y, str = 100, 200, "Sum of %d and %d is %d\n"
+			result    = x + y
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	x, y, str := 100, 200, "Sum of %d and %d is %d\n"
+	result := x + y
+	fmt.Printf(str, x, y, result)
 
 }
