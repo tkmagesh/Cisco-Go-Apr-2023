4cebc19954b396bd535913ffabbce0d0e58d923d
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-03 12:52:27 +0530

    iota

---
+++ 01-intro/05-iota.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		const red int = 0
+		const green int = 1
+		const blue int = 2
+	*/
+
+	/*
+		const (
+			red   int = iota
+			green int = iota
+			blue  int = iota
+		)
+	*/
+
+	/*
+		const (
+			red   int = iota
+			green     = iota
+			blue      = iota
+		)
+	*/
+	/*
+		const (
+			red int = iota
+			green
+			blue
+		)
+	*/
+
+	/*
+		const (
+			red = iota + 2
+			green
+			blue
+		)
+	*/
+
+	/*
+		const (
+			red = iota * 2
+			green
+			blue
+		)
+	*/
+
+	const (
+		red = iota * 2
+		green
+		_
+		blue
+	)
+	fmt.Printf("Red = %d, Greent = %d and Blue = %d\n", red, green, blue)
+
+	fmt.Println("iota applied")
+	const (
+		VERBOSE = 1 << iota
+		CONFIG_FROM_DISK
+		DATABASE_REQUIRED
+		LOGGER_ACTIVATED
+		DEBUG
+		FLOAT_SUPPORT
+		RECOVERY_MODE
+		REBOOT_ON_FAILURE
+	)
+	fmt.Printf("%b, %b, %b, %b, %b, %b, %b, %b\n", VERBOSE, CONFIG_FROM_DISK, DATABASE_REQUIRED, LOGGER_ACTIVATED, DEBUG, FLOAT_SUPPORT, RECOVERY_MODE, REBOOT_ON_FAILURE)
+}
