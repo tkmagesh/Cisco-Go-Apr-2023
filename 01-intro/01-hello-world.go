ff30ef2f3ce7f8ce02f5e2421c61387746aae277
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-03 10:49:17 +0530

    hello world in go

---
+++ 01-intro/01-hello-world.go
@@ -0,0 +1,16 @@
+// package declaration
+package main
+
+// import dependency packages
+
+// package level variable declarations & type definitions
+
+// package init function
+
+// main function
+func main() {
+	// print("Hello World!\n")
+	println("Hello World!")
+}
+
+// other functions
