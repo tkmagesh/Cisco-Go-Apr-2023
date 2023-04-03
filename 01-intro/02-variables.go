20ffa05b9529261dc5de9d939171fafbfc544803
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-03 12:07:41 +0530

    Function Scope vs Package scoped variables

---
+++ 01-intro/02-variables.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+var x = 100
+
+func main() {
+	/*
+		var name string
+		name = "Magesh"
+		fmt.Printf("Hi %s!\n", name)
+	*/
+
+	/*
+		var name string = "Magesh"
+		fmt.Printf("Hi %s!\n", name)
+	*/
+
+	//type inference
+	/*
+		var name = "Magesh"
+		fmt.Printf("Hi %s!\n", name)
+	*/
+
+	name := "Magesh"
+	fmt.Printf("Hi %s!\n", name)
+
+}
