98dad6141ff9b97be9730f7b5c8c675536a23051
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-04 12:26:30 +0530

    slice - introduction

---
+++ 08-collections/02-slice.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nos []int
+	// var nos []int = []int{3, 1, 4, 2, 5}
+	// var nos = []int{3, 1, 4, 2, 5}
+	nos := []int{3, 1, 4, 2, 5}
+	fmt.Println(nos)
+
+	fmt.Println("len(nos) =", len(nos))
+
+	fmt.Println()
+	fmt.Println("Iterating a slice [using indexer]")
+	for i := 0; i < len(nos); i++ {
+		fmt.Printf("nos[%d] = %d\n", i, nos[i])
+	}
+
+	fmt.Println()
+	fmt.Println("Iterating a slice [using range]")
+	for idx, val := range nos {
+		fmt.Printf("nos[%d] = %d\n", idx, val)
+	}
+
+	fmt.Println("Appending new values")
+	nos = append(nos, 10)
+	fmt.Println(nos)
+	nos = append(nos, 20, 30)
+	fmt.Println(nos)
+
+	fmt.Println("Appending another slice")
+	hundreds := []int{100, 200, 300}
+	nos = append(nos, hundreds...)
+	fmt.Println(nos)
+
+	fmt.Println("Slicing")
+	fmt.Println("nos[2:5] =", nos[2:5])
+	fmt.Println("nos[2:] =", nos[2:])
+	fmt.Println("nos[:5] =", nos[:5])
+}
