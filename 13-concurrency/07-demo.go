edf554d7544be60ca35da85d159d031e6c85b3fb
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-05 17:04:01 +0530

    http services - introduction

--- 13-concurrency/07-demo.go
+++ 13-concurrency/07-demo.go
@@ -6,6 +6,7 @@ import (
 )
 
 //communicate by sharing memory (NOT ADVISABLE)
+/*
 var result int
 
 func main() {
@@ -19,4 +20,19 @@ func main() {
 func add(x, y int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	result = x + y
+}
+*/
+
+func main() {
+	wg := &sync.WaitGroup{}
+	var result int
+	wg.Add(1)
+	go add(100, 200, wg, &result)
+	wg.Wait()
+	fmt.Println(result)
+}
+
+func add(x, y int, wg *sync.WaitGroup, result *int) {
+	defer wg.Done()
+	*result = x + y
 }
