e5a9d91a815fdd4f11ff204558d03b2504f42bf8
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-05 15:18:59 +0530

    bug fix

--- 13-concurrency/08-a-demo.go
+++ 13-concurrency/08-a-demo.go
@@ -12,13 +12,13 @@ func main() {
 	ch := make(chan int)
 	wg.Add(1)
 	go add(100, 200, wg, ch)
-	wg.Wait()
 	result := <-ch
+	wg.Wait() // blocked
 	fmt.Println(result)
 }
 
 func add(x, y int, wg *sync.WaitGroup, ch chan int) {
-	defer wg.Done()
 	result := x + y
-	ch <- result
+	ch <- result //non-blocking
+	wg.Done()
 }
---
+++ 13-concurrency/08-b-demo.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Share memory by communicating
+
+func main() {
+
+	ch := make(chan int)
+	go add(100, 200, ch)
+	result := <-ch
+	fmt.Println(result)
+}
+
+func add(x, y int, ch chan int) {
+	result := x + y
+	ch <- result //non-blocking
+}
